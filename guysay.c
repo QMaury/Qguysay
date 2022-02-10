c6d0ea5eb2fd91b59241f0c616c56a7ffed52de3
QMaury <87496773+QMaury@users.noreply.github.com>
2022-02-09 19:42:42 -0800

    yet more optimizations

--- guysay.c
+++ guysay.c
@@ -1,19 +1,9 @@
-/* Guysay.c
- * this program is to get a stickman to echo a given phrasea
- * QMaury
- */
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 int main(int argc, char *argv[]) {
         int i;
         ++i;
-        if(argv[i] == NULL) {
+        if(argv[i] == NULL) 
                 argv[i] = "ERROR! NO ARGUMENTS PROVIDED!";
-        }
-	printf("\n");
-	printf("        %s\n", argv[i]);
-	printf("       /\n      /\n   0   \n  \\|   \n   |\\\n   |  \n  / \\\n");
+	printf("\n        %s\n", argv[i]);
+        printf("       /\n      /\n   0   \n  \\|   \n   |\\\n   |  \n  / \\\n");
 }
